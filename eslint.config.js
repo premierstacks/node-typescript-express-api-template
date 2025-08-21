60673043caa0d08fe8b554adbfd47c329281f22d
Tomáš Chochola <chocholatom1997@gmail.com>
2025-08-21 22:23:49 +0200

    Sync with @premierstacks:#main

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,18 @@
 import { EslintStack } from '@premierstacks/eslint-stack';
+import globals from 'globals';
 
 // eslint-disable-next-line no-restricted-exports
-export default EslintStack.Presets.nodeTypescript().build();
+export default EslintStack.create()
+  .base()
+  .globals({
+    ...globals.node,
+    ...globals.es2024,
+  })
+  .ignores([...EslintStack.Selectors.GlobalIgnore])
+  .ignores(['dist'])
+  .recommended()
+  .typescript({ files: [...EslintStack.Selectors.GlobalTypeScript, ...EslintStack.Selectors.GlobalTypedReact] })
+  .stylistic()
+  .sonarjs()
+  .typescriptDisabled({ files: [...EslintStack.Selectors.GlobalJavaScript, ...EslintStack.Selectors.GlobalReact] })
+  .build();
